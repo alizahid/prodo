4520e9c17c502a203c2d99fb6b04310c8186ebcb
Ali Zahid <ali.zahid@live.com>
2019-09-29 15:46:23 +0400

    Bug fix

--- src/scenes/create/index.tsx
+++ src/scenes/create/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect } from 'react'
 import { RouteComponentProps } from 'react-router'
 
 import { Form } from '../../components'
@@ -7,7 +7,15 @@ import { Main } from './components'
 
 export const Create: FunctionComponent<RouteComponentProps> = ({ history }) => {
   const { create } = useStoreActions(actions => actions.snippets)
+  const { setSnippetId } = useStoreActions(actions => actions.state)
   const { saving } = useStoreState(state => state.snippets)
+  const { snippetId } = useStoreState(state => state.state)
+
+  useEffect(() => {
+    if (snippetId) {
+      setSnippetId(null)
+    }
+  }, [snippetId, setSnippetId])
 
   return (
     <Main>
